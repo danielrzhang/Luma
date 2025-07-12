a97a3dd246cefce7e6b2c52af2be6b4d442f9521
Daniel Zhang <danielrzhang@gmail.com>
2025-07-12 11:42:44 -0400

    Show generated interviews on the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 import { dummyInterviews } from '@/constants'
+import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser()
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({userId: user?.id!})
+  ])
+
+  const hasPastInterviews = userInterviews?.length > 0
+  const hasUpcomingInteviews = latestInterviews?.length > 0
+
   return (
     <>
       <section className="card-cta">
@@ -27,21 +38,26 @@ const Page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))) : (
+              <p>You haven&apos;t taken any interviews yet</p>  
+          )}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
+          {hasUpcomingInteviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))) : (
+              <p>There are no new interviews available.</p>  
+          )}
         </div>
 
-        {/* <p>You haven&apos;t taken any interviews yet</p> */}
       </section>
     </>
   )
